a142854cd6b2946a079d4c09dde1e4bfce1b9784
RISHAB KUMAR <129298737+rishabmahor06@users.noreply.github.com>
2025-11-28 15:54:17 +0530

    Update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', async (event) => {
   const data = { fullName, email, projectType, mobile, projectDetails };
 
   try {
-    const response = await fetch('http://localhost:3000/api/contact', {
+    const response = await fetch('https://wixel-backend-1.onrender.com/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
